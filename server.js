03691a262d075f91387f0d06987084793b313590
Lombardo <lomba97@gmail.com>
2017-12-21 13:59:06 -0500

    Server

--- server.js
+++ server.js
@@ -14,22 +14,10 @@ client.connect();
 //Create the express app
 const app = express();
 
-//Use body-parser as middleware. Parse the body as JSON
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
 //Handle a post request at /query
 app.post('/query', (req, res) => {
     console.log("Receiving request")
-    console.log(req);
-    let query = req.body.query;
-
-    //Send the query and return the result as  JSON
-    client.query(query, (err, r) => {
-        if (err) console.log("error");
-
-        res.end(JSON.stringify(r.rows));
-    });
+    console.log(req.body);
 
 });
 
